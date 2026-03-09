20370753c0a6067aada97ea3e7b38d90cf2f9d00
Ashwin Kumar <BokuWaAshwin@gmail.com>
2026-03-09 22:25:44 +0530

    fix/sslmode-error

--- app/db/session.py
+++ app/db/session.py
@@ -1,7 +1,10 @@
-﻿from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from app.core.config import settings
 
-engine = create_async_engine(settings.DATABASE_URL)
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    connect_args={"ssl": True},
+)
 SessionLocal = async_sessionmaker(
     autocommit=False,
     autoflush=False,
